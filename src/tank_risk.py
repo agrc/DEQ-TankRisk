a0d6eae55eed41f699a7db8e4cd7fde1635248f8
kwalker9 <klwalker9@gmail.com>
2015-09-29 20:33:05 -0600

    Output field headers now match layer order. Issue #3

--- src/tank_risk.py
+++ src/tank_risk.py
@@ -149,21 +149,24 @@ class TankResult(object):
         """Get a list that contains the rows of the output table, including header.
         - featureNames are used to order output table fields."""
         outputRows = []
-        featureNameRowOrder = list(featureNames)
+        featureNameOrder_Header = list(featureNames)
+        featureNamesOrder_Values = []
+        
         headerList = [TankResult.OUTPUT_ID_FIELD]
-        for f in featureNameRowOrder:
+        for f in featureNameOrder_Header:
             valFieldName = TankResult.layerNames[f].valFieldName
             sevFieldName = TankResult.layerNames[f].sevFieldName
             if valFieldName in headerList or sevFieldName in headerList:
-                featureNameRowOrder.remove(f)#Two layers can share one output field and the field doesn't need to be added to twice.
-                continue
+                continue#Two layers can share one output field and the field doesn't need to be added to twice.
+            featureNamesOrder_Values.append(f)#Make a list without shared outputs for value adding efficiency
             headerList.append(valFieldName)
             headerList.append(sevFieldName)
+
         outputRows.append(headerList)
-            
+                    
         for t in TankResult.tankResults.values():
             tempValueList = [t.tankId]
-            for f in featureNameRowOrder:
+            for f in featureNamesOrder_Values:
                 tempValueList.append(t.__getattribute__(TankResult.layerNames[f].valAttribute))
                 tempValueList.append(t.__getattribute__(TankResult.layerNames[f].sevAttribute))
             outputRows.append(tempValueList)
