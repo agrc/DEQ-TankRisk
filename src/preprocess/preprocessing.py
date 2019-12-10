6ba31d2e6d384f2b3a14c8508d2bd4f23a22a3d6
steveoh <sgourley@utah.gov>
2019-12-10 16:43:31 -0700

    refactor: pathlib instead of hardcoded items

--- src/preprocess/preprocessing.py
+++ src/preprocess/preprocessing.py
@@ -7,14 +7,14 @@ A module that adds layers to a map
 
 
 import os
+import pathlib
 import time
+import sys
+
+from tqdm import tqdm
 
 import arcpy
 
-output_dir = r'C:\Users\kwalker\Documents\Aptana Studio 3 Workspace\DEQ-TankRisk\data\srcdata.gdb'
-input_dir = r'Database Connections\agrc@SGID10@gdb10.agrc.utah.gov.sde'  #: Outside state network path issue
-
-nearTablePrefix = 'Near_'
 tank_layer = 'SGID10.ENVIRONMENT.FACILITYUST'
 risk_layers = [
     'SGID10.ENVIRONMENT.DWQAssessedWaters',
@@ -29,19 +29,33 @@ risk_layers = [
 ]
 
 
-def add_layers_to_map(mxd_path):
-    mxd = arcpy.mapping.MapDocument(mxd_path)
+def add_layers_to_map(project_path, workspace):
+    import pdb; pdb.set_trace()
 
-    for risk in risk_layers:
-        riskLayer = arcpy.mapping.Layer(os.path.join(inputDir, risk))
-        print(riskLayer)
-        arcpy.mapping.AddLayer(arcpy.mapping.ListDataFrames(mxd)[0], riskLayer)
+    project = arcpy.mp.ArcGISProject(project_path)
+    risk_map = project.listMaps('RiskMap')[0]
 
-    mxd.save()
+    for layer_name in tqdm(risk_layers):
+        risk_map.addDataFromPath(os.path.join(workspace, layer_name))
 
-    del mxd
+    project.save()
 
 
 if __name__ == '__main__':
-    mxdPath = r'C:\Users\Administrator\My Documents\Aptana Studio 3 Workspace\DEQ-TankRisk\data\test_map.mxd'
-    add_layers_to_map(mxdPath)
+    project_root = pathlib.Path(__file__).resolve().parent.parent.parent
+
+    pro_project_dir = project_root.joinpath('proproject')
+    pro_project = pro_project_dir.joinpath('TankRisk', 'TankRisk.aprx')
+    sde_path = pro_project_dir.joinpath('sgid.agrc.utah.gov.sde')
+
+    if not pro_project.exists():
+        print(f'could not find pro project in {pro_project}')
+
+        sys.exit(-1)
+
+    if not sde_path.exists():
+        print(f'could not find sgid10 sde in {sde_path}')
+
+        sys.exit(-1)
+
+    add_layers_to_map(str(pro_project), str(sde_path))
