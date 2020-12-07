b1405c3839760c8ea7318ff5c40d6c8003a6f35a
steveoh <sgourley@utah.gov>
2020-12-07 16:27:25 -0700

    chore: update cli to run tool

--- src/tank_risk_cli.py
+++ src/tank_risk_cli.py
@@ -23,15 +23,64 @@ class Logger():
     def AddErrorMessage(self, string):
         print(string)
 
+    def AddError(self, string):
+        print(string)
+
+layer_lookup = {
+    'https://services1.arcgis.com/99lidPhWCzftIe9K/arcgis/rest/services/FACILITYUST/FeatureServer/0': 'tanks',
+    'deq_underground_storage_tanks': 'tanks',
+    'https://services.arcgis.com/ZzrwjTRez6FJiOq4/arcgis/rest/services/PODView/FeatureServer/0': 'points_of_diversion',
+    'https://services1.arcgis.com/99lidPhWCzftIe9K/arcgis/rest/services/Utah_Soils/FeatureServer/0': 'soil',
+    'aquifer_recharge_discharge_areas': 'aquifer_recharge_discharge_areas',
+    'wetlands': 'wetlands',
+    'dwq_assessment_units': 'dwq_assessment_units',
+    'shallow_ground_water': 'shallow_ground_water',
+    'census_tracts_2010': 'census_tracts_2010',
+    'lakes_nhd': 'lakes_nhd',
+    'streams_nhd': 'streams_nhd',
+    'dwq_assessment_units': 'dwq_assessment_units'
+}
+
+def find_tank_layer(layers):
+    layer_links = {
+
+    }
+
+    for layer in layers:
+        if not layer.isFeatureLayer:
+            continue
+
+        if layer.isWebLayer:
+            if(layer.dataSource in layer_lookup):
+                layer_links[layer_lookup[layer.dataSource]] = layer
+
+            continue
+
+        props = layer.connectionProperties
+        if props['workspace_factory'] != 'SDE':
+            continue
+
+        if props['connection_info']['db_connection_properties'] != 'opensgid.agrc.utah.gov':
+            continue
+
+        dataset = props['dataset']
+        table_name = dataset.split('.')[-1:][0].strip('%')
+
+        if(table_name in layer_lookup):
+            layer_links[layer_lookup[table_name]] = layer
+
+    return layer_links
+
 def test():
     project_root = pathlib.Path(__file__).resolve().parent.parent
     pro_project_dir = project_root.joinpath('proproject')
     pro_project = str(pro_project_dir.joinpath('TankRisk', 'TankRisk.aprx'))
-    map_name = 'Layers'
+    map_name = 'RiskMap'
 
-    risk_map = arcpy.mp.ArcGISProject(pro_project).listMaps(map_name)[0]
-
-    facility_ust_points = risk_map.listLayers('SGID10.ENVIRONMENT.FACILITYUST')[0]
+    risk_project = arcpy.mp.ArcGISProject(pro_project)
+    risk_map = risk_project.listMaps(map_name)[0]
+    layers = find_tank_layer(risk_map.listLayers())
+    facility_ust_points = layers['tanks']
     output_directory = str(pro_project_dir.joinpath('outputs'))
 
     print(f'Version {VERSION}')
